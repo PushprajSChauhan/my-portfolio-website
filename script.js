4a0d984b1d5f1c3f8be4dcf91e0cdf4f0002e9ae
Pushpraj Singh Chauhan <138218142+PushprajSChauhan@users.noreply.github.com>
2024-04-21 10:49:11 +0530

    navbar color commit

--- script.js
+++ script.js
@@ -41,3 +41,13 @@ ScrollReveal().reveal(
 ScrollReveal().reveal(".about-col-2 .content, .skill-right", {
   origin: "right",
 });
+
+const menuIcon = document.querySelector("#menu-icon");
+const navbar = document.querySelector(".navbar");
+const navbg = document.querySelector(".nav-bg");
+
+menuIcon.addEventListener("click", () => {
+  menuIcon.classList.toggle("fa-xmark");
+  navbar.classList.toggle("active");
+  navbg.classList.toggle("active");
+});
